ab842c507bee4e658787ab600fe8fa4789cf2857
yalfmon0307 <montesyalfred0@gmail.com>
2025-03-16 18:31:28 -0600

    creando los controller

---
+++ backend/routes/routes.js
@@ -0,0 +1,29 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/", (req, res) => {
+    res.json({message: "Hello World" });
+});
+
+router.post("/login", (req, res) => {
+    res.json({message: "Login" });
+});
+
+router.post("/register", (req, res) => {
+    res.json({message: "Register" });
+});
+
+router.post("/get_data", (req, res) => {
+    res.json({message: "Get Data" });
+});
+
+router.post("/update_data", (req, res) => {
+    res.json({message: "Update Data" });
+});
+
+router.get("/get_data_country", (req, res) => {
+    res.json({message: "Get Data Country" });
+});
+
+export default router;
